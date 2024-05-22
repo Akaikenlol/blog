a4c209d97858dc113ef4fc9c5be445c3ba9ee55e
Akaikenlol <keithken972@gmail.com>
2024-05-22 21:44:29 +0630

    feat: Implementing Mobile Nav and modifying Navbar chore: npm install sheet, logo add

--- components/Container.tsx
+++ components/Container.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Container = ({ children }: { children: React.ReactNode }) => {
 	return (
-		<div className="max-w-[1350px] mx-auto bg-white min-h-screen flex flex-col">
+		<div className="max-w-[1350px] mx-auto bg-white min-h-screen flex flex-col border-l border-r">
 			{children}
 		</div>
 	);
